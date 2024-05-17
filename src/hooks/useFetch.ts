bcb564e4be765aa06877d04292fffe9df6494111
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-05-17 09:35:22 +0200

    fix([Hook]Fetch): Fix fetch adding useCallback for stabilized usefetch return

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,34 +1,38 @@
 import { useAuth } from "@/contexts/AuthProvider";
+import { useCallback } from "react";
 
 const useFetch = (path: string) => {
-    const { auth } = useAuth();
-    const baseUrl = 'http://localhost:5001/api';
+  const { auth } = useAuth();
+  const baseUrl = "http://localhost:5001/api";
 
-    const fetchWithAuth = async (options: RequestInit = {}) => {
-        try {
-            const response = await fetch(`${baseUrl}${path}`, {
-                ...options,
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...(auth?.token ? { Authorization: `Bearer ${auth.token}` } : {}),
-                    ...options.headers,
-                },
-            });
+  const fetchWithAuth = useCallback(
+    async (options: RequestInit = {}) => {
+      try {
+        const response = await fetch(`${baseUrl}${path}`, {
+          ...options,
+          headers: {
+            "Content-Type": "application/json",
+            ...(auth?.token ? { Authorization: `Bearer ${auth.token}` } : {}),
+            ...options.headers,
+          },
+        });
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.error);
-            }
-
-            const data = await response.json();
-            return data;
-        } catch (err) {
-            console.error("err", err);
-            throw err;
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.error);
         }
-    };
 
-    return fetchWithAuth;
+        const data = await response.json();
+        return data;
+      } catch (err) {
+        console.error("err", err);
+        throw err;
+      }
+    },
+    [auth?.token, path]
+  );
+
+  return fetchWithAuth;
 };
 
 export default useFetch;
