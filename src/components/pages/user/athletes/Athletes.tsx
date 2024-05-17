0f280ed58122b45af67feccc833234e67dfd1d5e
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2024-05-17 15:21:26 +0200

    feat(athletes): Cards UI

--- src/components/pages/user/athletes/Athletes.tsx
+++ src/components/pages/user/athletes/Athletes.tsx
@@ -1,5 +1,5 @@
 import useFetch from "@/hooks/useFetch";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import AthleteCard from "./athleteCard/AthleteCard";
 import Header from "@/components/sections/Header/Header";
 import { useToast } from "@/components/ui/use-toast";
@@ -9,16 +9,13 @@ import PageTemplate from "@/components/sections/PageTeample/PageTemplate";
 
 export default function Athletes() {
   const { toast } = useToast();
-  const {
-    data: athletes,
-    isLoading,
-    error,
-    fetchData: fetchAthletes,
-  } = useFetch<Athlete[]>("/athletes");
+  const [athletes, setAthletes] = useState<Athlete[]>([]);
+  const { isLoading, error, fetchData: fetchAthletes } = useFetch("/athletes");
 
   const getAthletes = useCallback(async () => {
     try {
-      await fetchAthletes({ method: "GET" });
+      const { data } = await fetchAthletes({ method: "GET" });
+      setAthletes(data);
     } catch (error) {
       toast({
         variant: "destructive",
@@ -31,6 +28,10 @@ export default function Athletes() {
   useEffect(() => {
     getAthletes();
   }, [getAthletes]);
+
+  const goToAthleteDetails = () => {
+    console.log("Go to athlete details");
+  };
 
   return (
     <div>
@@ -45,7 +46,11 @@ export default function Athletes() {
             ? athletes?.map((athlete) => {
                 return (
                   <GridItem columnSpan={4}>
-                    <AthleteCard key={athlete.id} athlete={athlete} />
+                    <AthleteCard
+                      key={athlete.id}
+                      athlete={athlete}
+                      onClick={goToAthleteDetails}
+                    />
                   </GridItem>
                 );
               })
--- src/components/pages/user/athletes/athleteCard/AthleteCard.tsx
+++ src/components/pages/user/athletes/athleteCard/AthleteCard.tsx
@@ -6,9 +6,10 @@ import athlete_m from "@/assets/images/athlete_m.png";
 
 interface AthleteCardProps {
   athlete: Athlete;
+  onClick?: () => void;
 }
 
-const AthleteCard = ({ athlete }: AthleteCardProps) => {
+const AthleteCard = ({ athlete, onClick }: AthleteCardProps) => {
   console.log("firstname", athlete.firstname);
 
   const addAthleteToLikes = () => {
@@ -22,46 +23,51 @@ const AthleteCard = ({ athlete }: AthleteCardProps) => {
     : athlete_m;
 
   return (
-    <div className={styles.athlete}>
-      <img
-        className={styles.athlete__pictureProfile}
-        src={profilePicture}
-        alt={`Photo of ${athlete.firstname} ${athlete.lastname}`}
-      />
-      <div className={styles.athlete__info_section}>
-        <div className={styles.athlete__top_section}>
-          <p className={styles.athlete__title}>
-            {athlete.firstname} {athlete.lastname}
-          </p>
-          <span className={styles.athlete__tooltip}>
-            {athlete.firstname} {athlete.lastname}
-          </span>
-          <p className={styles.athlete__subtitle}>NB medals</p>
-        </div>
-        <div>
-          <p className={styles.athlete__category_title}>
-            Sport:{" "}
-            <span className={styles.athlete__category}>
-              {athlete?.athleteSports.name}
+    <div className={styles.card_container}>
+      {/* Like button */}
+      <div className={styles.like_btn}>
+        <button aria-label="Like" onClick={addAthleteToLikes}>
+          <HeartIcon />
+        </button>
+      </div>
+      <div className={styles.athlete} onClick={onClick}>
+        <img
+          className={styles.athlete__pictureProfile}
+          src={profilePicture}
+          alt={`Photo of ${athlete.firstname} ${athlete.lastname}`}
+        />
+        <div className={styles.athlete__info_section}>
+          <div className={styles.athlete__top_section}>
+            <p className={styles.athlete__title}>
+              {athlete.firstname} {athlete.lastname}
+            </p>
+            <span className={styles.athlete__tooltip}>
+              {athlete.firstname} {athlete.lastname}
             </span>
-          </p>
-          <p className={styles.athlete__category_title}>
-            Nationality:{" "}
-            <span className={styles.athlete__flag}>{athlete.country.iso}</span>
-          </p>
-          <p className={styles.athlete__category_title}>
-            Age: <span className={styles.athlete__category}>{athlete.age}</span>
-          </p>
-          <p className={styles.athlete__category_title}>
-            Gender:{" "}
-            <span className={styles.athlete__category}>{athlete.gender}</span>
-          </p>
-        </div>
-        {/* Like button */}
-        <div className={styles.athlete__like_btn}>
-          <button aria-label="Like" onClick={addAthleteToLikes}>
-            <HeartIcon />
-          </button>
+            <p className={styles.athlete__subtitle}>NB medals</p>
+          </div>
+          <div>
+            <p className={styles.athlete__category_title}>
+              Sport:{" "}
+              <span className={styles.athlete__category}>
+                {athlete?.athleteSports.name}
+              </span>
+            </p>
+            <p className={styles.athlete__category_title}>
+              Nationality:{" "}
+              <span className={styles.athlete__flag}>
+                {athlete.country.iso}
+              </span>
+            </p>
+            <p className={styles.athlete__category_title}>
+              Age:{" "}
+              <span className={styles.athlete__category}>{athlete.age}</span>
+            </p>
+            <p className={styles.athlete__category_title}>
+              Gender:{" "}
+              <span className={styles.athlete__category}>{athlete.gender}</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
