2827b9fd41db43c17ad9158ae5f2391470098205
samuelaudic <samuelaudic@gmail.com>
2024-05-17 14:26:46 +0200

    component(MedalProfile): create component V1 + json temp data

--- src/components/pages/user/dashboard/UserDashboard.tsx
+++ src/components/pages/user/dashboard/UserDashboard.tsx
@@ -1,16 +1,25 @@
 import Header from "@/components/sections/Header/Header";
 import PageTemplate from "@/components/sections/PageTeample/PageTemplate";
+import MedalProfile from "@/components/ui/MedalProfile/MedalProfile";
+import athletesData from "../../../ui/MedalProfile/temp__athlete.json";
 
 export default function UserDashboard() {
-    return (
-        <>
-            <Header
-                title="Dashboard of Paris 2024 live data"
-                subtitle="Description for amazing dashboard in Paris 2024 with all KPI"
-            />
-            <PageTemplate>
-                <h1>Page content</h1>
-            </PageTemplate>
-        </>
-    )
+  const athletes = athletesData.athletes;
+
+  return (
+    <>
+      <Header
+        title="Dashboard of Paris 2024 live data"
+        subtitle="Description for amazing dashboard in Paris 2024 with all KPI"
+      />
+      <PageTemplate>
+        <h1>Page content</h1>
+        <div className="flex gap-[2rem]">
+          <MedalProfile type={1} athlete={athletes[0]} />
+          <MedalProfile type={2} athlete={athletes[1]} />
+          <MedalProfile type={3} athlete={athletes[2]} />
+        </div>
+      </PageTemplate>
+    </>
+  );
 }
---
+++ src/components/ui/MedalProfile/MedalProfile.tsx
@@ -0,0 +1,71 @@
+import styles from "./MedalProfile.module.scss";
+
+interface MedalProfileProps {
+  type: number;
+  athlete: {
+    id: number;
+    name: string;
+    country: string;
+    sport: string;
+    profile: string;
+    totalMedals: number;
+  };
+}
+
+function MedalProfile({ type, athlete }: MedalProfileProps) {
+  let medalColorTxt;
+  let medalColorBg;
+  switch (type) {
+    case 1:
+      medalColorTxt = "#524709";
+      medalColorBg = "#FFD700";
+      break;
+    case 2:
+      medalColorTxt = "#595959";
+      medalColorBg = "#C0C0C0";
+      break;
+    case 3:
+      medalColorTxt = "#524709";
+      medalColorBg = "#CD7F32";
+      break;
+    default:
+      medalColorTxt = "#000";
+      medalColorBg = "#fff";
+  }
+
+  return (
+    <div className={styles.medalProfile}>
+      <div className={styles.medalProfile__content}>
+        <div className={styles.medalProfile__content__medal}>
+          <div
+            className={styles.medalProfile__content__medal__header}
+            style={{ backgroundColor: medalColorBg, color: medalColorTxt }}
+          >
+            <span>{type}</span>
+          </div>
+          <div
+            className={styles.medalProfile__content__medal__picture}
+            style={{ border: `8px solid ${medalColorBg}` }}
+          >
+            <img src={athlete.profile} alt={`Picture of ${athlete.name}`} />
+          </div>
+          <div
+            className={styles.medalProfile__content__medal__footer}
+            style={{ backgroundColor: medalColorBg, color: medalColorTxt }}
+          >
+            <span>{athlete.totalMedals} medals</span>
+          </div>
+        </div>
+        <h3>{athlete.name}</h3>
+        <p>
+          Sport : <span>{athlete.sport}</span>
+        </p>
+        <p>
+          Nationality : <span>{athlete.country}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default MedalProfile;
