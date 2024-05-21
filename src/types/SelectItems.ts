8bbee5785b0df33ead337ade77f797eb2342d09c
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-05-21 20:42:15 +0200

    feat(countries): [Select] Add a continents select for the filter

---
+++ src/types/SelectItems.ts
@@ -0,0 +1,40 @@
+export interface Item {
+  value: string;
+  label: string;
+}
+export type SelectItems = Item[];
+
+export const continentItems: SelectItems = [
+  {
+    value: "0",
+    label: "All",
+  },
+  {
+    value: "1",
+    label: "Africa",
+  },
+  {
+    value: "2",
+    label: "Antartica",
+  },
+  {
+    value: "3",
+    label: "Asia",
+  },
+  {
+    value: "4",
+    label: "Europe",
+  },
+  {
+    value: "5",
+    label: "North America",
+  },
+  {
+    value: "6",
+    label: "Australia",
+  },
+  {
+    value: "7",
+    label: "South America",
+  },
+];
