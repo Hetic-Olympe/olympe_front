8928daf3c3324c07587398749d171197feb60631
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-05-22 22:56:42 +0200

    refactor(adminUserManager): form component with utils zod

--- src/components/routing/admin/AdminDashboardRouter.tsx
+++ src/components/routing/admin/AdminDashboardRouter.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import NotFound from "../../sections/NotFound/NotFound";
 import AdminDashboard from "@/components/pages/admin/dashboard/AdminDashboard";
-import UserEdit from "@/components/pages/admin/dashboard/AdminUserEdit/UserEdit";
+import AdminUserDetail from "@/components/pages/admin/dashboard/AdminUserDetail/AdminUserDetail";
 
 export default function AdminDashboardRouter() {
     return (
         <Routes>
             <Route path="/" element={<AdminDashboard />} />
-            <Route path="/user/:id" element={<UserEdit />} />
+            <Route path="/user/:id" element={<AdminUserDetail />} />
             <Route path="/*" element={<NotFound />} />
         </Routes>
     );
--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const zNotNullStringSchema = (field: string) => z.string().nullish().refine(value => value !== null, {
+  message: `${field} cannot be null`,
+});
