a8c4d91574258b606ae4f59b75a8091933212ad1
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-05-23 09:23:14 +0200

    fix(auth): security token stockage

--- src/contexts/AuthProvider.tsx
+++ src/contexts/AuthProvider.tsx
@@ -1,34 +1,49 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie'
+
+interface AuthDataType {
+  username: string | null;
+  role: string | null;
+  token: string | null;
+}
+
 interface AuthContextType {
   isAuthenticated: boolean;
   role: string | null;
-  auth: { username: string; role: string; token: string } | null;
+  auth: AuthDataType | null;
   signIn: (username: string, role: string, token: string) => void;
   signOut: () => void;
 }
 
+const AUTH_COOKIE_KEYS = ['username', 'role', 'token'];
+
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const AuthProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const initialAuthState = {
+    username: Cookies.get('username') || null,
+    role: Cookies.get('role') || null,
+    token: Cookies.get('token') || null,
+  };
 
-  // Initialize auth state from localStorage
-  const storedAuth = localStorage.getItem('auth');
-  const initialAuthState = storedAuth ? JSON.parse(storedAuth) : null;
-  const [auth, setAuth] = useState<{ username: string; role: string; token: string } | null>(initialAuthState);
+  const [auth, setAuth] = useState<AuthDataType | null>(initialAuthState.token ? initialAuthState : null);
 
   const signIn = (username: string, role: string, token: string) => {
-    const newAuth = { username, role, token };
-    setAuth(newAuth);
-    localStorage.setItem('auth', JSON.stringify(newAuth));
+    setAuth({ username, role, token });
+    AUTH_COOKIE_KEYS.forEach(key => Cookies.set(key, eval(key), { secure: true, sameSite: 'Strict', httpOnly: true }));
   };
 
   const signOut = () => {
     setAuth(null);
-    localStorage.removeItem('auth');
+    AUTH_COOKIE_KEYS.forEach(key => Cookies.remove(key));
   };
 
+  useEffect(() => {
+    if (!auth && initialAuthState.token) setAuth(initialAuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated: auth !== null, role: auth?.role ?? null, auth, signIn, signOut }}>
+    <AuthContext.Provider value={{ isAuthenticated: !!auth, role: auth?.role ?? null, auth, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
@@ -36,8 +51,6 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
   return context;
 };
